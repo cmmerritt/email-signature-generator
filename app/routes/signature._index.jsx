2b876a556f46bff6ece1b5784ce3ec210e8fac80
cmmerritt <christiane.merritt@gmail.com>
2023-11-28 00:57:09 -0800

    add initial styling: fonts

--- app/routes/signature._index.jsx
+++ app/routes/signature._index.jsx
@@ -1,8 +1,6 @@
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
-// import { getSignatures } from "../models/signature.server";
-
 export const loader = async () => {
   const quoteRes = await fetch(
     `${process.env.API_NINJAS_QUOTES_BASE_PATH}/`,
@@ -24,34 +22,26 @@ export const loader = async () => {
     quoteRes: await quoteRes.json(),
     authorRes: await authorRes.json()
   });
-  // const signatures = await response.json();
-  // console.log("signatures from loader", signatures);
-  // return json({ signatures });
 };
-
-// export const loader = async () => {
-//   return json({ signatures: await getSignatures() });
-// };
 
 export default function Signature() {
   const { quoteRes, authorRes } = useLoaderData();
   return (
     <main>
       <h1>Your New Email Signature</h1>
-      <ul>
+
         {quoteRes.map((quote) => (
-          <div>
+          <div style={{ fontFamily: "Papyrus", fontSize: "2em" }}>
               {quote.quote}
           </div>
         ))}
-      </ul>
-      <ul>
+
         {authorRes.map((author) => (
-          <div>
+          <div style={{ fontFamily: "cursive", fontSize: "1.5em" }}>
               ~{author.author}
           </div>
         ))}
-      </ul>
+
     </main>
   );
 }
