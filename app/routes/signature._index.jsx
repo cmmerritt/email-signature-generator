c731c5fe435c9243bfdf02444b41155be75b177f
cmmerritt <christiane.merritt@gmail.com>
2024-01-06 20:17:28 -0800

    add authors, refactor quote display, add authorMatch function

--- app/routes/signature._index.jsx
+++ app/routes/signature._index.jsx
@@ -1,9 +1,92 @@
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
-const authors = ['Buddha', 'Mark Twain', 'Harriet Tubman', 'Marilyn Monroe', 'Albert Einstein', 'Wayne Gretzky', 'Wayne Gretzky ~Michael Scott', 'Susan B. Anthony', 'John F. Kennedy, Jr.', 'Helen Keller', 'Shaquille O\'Neal', 'Lao Tzu', 'Socrates', 'Leonardo da Vinci', 'Benjamin Franklin', 'Pope John Paul II', 'Beyonce', 'Martin Luther King, Jr.', 'Abraham Lincoln', 'W. E. B. DuBois', 'Oscar Wilde', 'Walt Disney', 'Kurt Vonnegut', 'Confucius', 'Maya Angelou', 'William Shakespeare', 'Virginia Woolf', 'Winston Churchill', 'Ayn Rand', 'Friedrich Nietzsche', 'Marie Antoinette', 'Barack Obama', 'Gandalf', 'Snoop Dogg'];
-
-//let randomAuthor = authors[Math.floor(Math.random()*authors.length)];
+const authors = [
+'Abraham Lincoln',
+'Albert Einstein',
+'Alexander the Great',
+'Alexandria Ocasio-Cortez',
+'Alice Walker',
+'Aristotle',
+'Ayn Rand',
+'Barack Obama',
+'Benjamin Franklin',
+'Beyoncé',
+'Bob Dylan',
+'Bruce Springsteen',
+'Buddha',
+'Charles Darwin',
+'Charles Dickens',
+'Cleopatra',
+'Confucius',
+'Dolly Parton',
+'Dr. Phil',
+'Dwayne "The Rock" Johnson',
+'Eleanor Roosevelt',
+'Emily Dickinson',
+'Eminem',
+'Friedrich Nietzsche',
+'Gandalf',
+'George Eliot',
+'George Washington',
+'Harriet Tubman',
+'Helen Keller',
+'Hillary Clinton',
+'Homer',
+'Homer Simpson',
+'Jack Kerouac',
+'Jane Austen',
+'Jay-Z',
+'Jean-Paul Sartre',
+'John F. Kennedy, Jr.',
+'John Lennon',
+'Julius Caesar',
+'Kesha',
+'Kurt Vonnegut',
+'Lao Tzu',
+'Leonardo da Vinci',
+'Lucius Annaeus Seneca',
+'Madonna',
+'Mahatma Gandhi',
+'Margaret Atwood',
+'Marie Antoinette',
+'Marilyn Monroe',
+'Mark Twain',
+'Martin Luther King, Jr.',
+'Maya Angelou',
+'Michelangelo',
+'Mother Teresa',
+'Muhammad Ali',
+'Oprah Winfrey',
+'Oscar Wilde',
+'Pope John Paul II',
+'Queen Elizabeth II',
+'Ralph Waldo Emerson',
+'Robin Williams',
+'Ruth Bader Ginsburg',
+'Saint Augustine',
+'Serena Williams',
+"Shaquille O\'Neal",
+'Snoop Dogg',
+'Socrates',
+'Stephen King',
+'Sylvia Plath',
+'Taylor Swift',
+'The Dalai Lama',
+'Thomas Aquinas',
+'Thomas Jefferson',
+'Toni Morrison',
+'Virgil',
+'Virginia Woolf',
+'Voltaire',
+'W. E. B. DuBois',
+'Walt Disney',
+'Walt Whitman',
+'Wayne Gretzky',
+'Wayne Gretzky ~Michael Scott',
+'William Shakespeare',
+'Winston Churchill'
+];
 
 export const loader = async () => {
   const quoteRes = await fetch(
@@ -25,19 +108,29 @@ export const loader = async () => {
 
 export default function Signature() {
   let { quoteRes, randomAuthor } = useLoaderData();
-  console.log(quoteRes);
-  console.log(randomAuthor);
+  let authorMatch = false;
+  if (quoteRes[0].author == randomAuthor) {
+    authorMatch = true;
+  };
+  console.log(quoteRes[0].author, randomAuthor)
   return (
     <main>
       <h1>Your New Email Signature</h1>
 
-        {quoteRes.map((quote) => (
-          <div style={{ fontFamily: "Papyrus", fontSize: "2em" }}>
-              {quote.quote} 
-          </div>
-        ))}
 
-        <div style={{ fontFamily: "cursive", fontSize: "1.5em" }}>~{randomAuthor}</div>
+        <div style={{ fontFamily: "Papyrus", fontSize: "2em" }}>
+              {quoteRes[0].quote} 
+          </div>
+
+        <div style={{ fontFamily: "cursive", fontSize: "1.5em" }}>
+              ~{randomAuthor}
+          </div>
+
+        { authorMatch && 
+          <div>
+            Whoa! This is the real author!
+          </div>
+        }
 
     </main>
   );
