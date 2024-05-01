cbb10b9f0d95a9f7113f937b199e4688027ad29c
cmmerritt <christiane.merritt@gmail.com>
2024-04-30 21:09:36 -0700

    adjust quote styling

--- app/routes/signature._index.jsx
+++ app/routes/signature._index.jsx
@@ -130,9 +130,9 @@ export default function Signature() {
   for(let i = 0; i < quoteWords.length; i++) {
     const wordNoPunct = quoteWords[i].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
     if(tokens.includes(wordNoPunct)) {
-      renderedQuote.push(<span key={i} sx={{ fontWeight: 'bold', color: `${userColor}`, fontFamily: `${userFont}`, textTransform: 'none', m: '1', p: '0', minWidth: '0' }} value={quoteWords[i]} onClick={handleNewCategoryChoice}>{quoteWords[i]} </span>);
+      renderedQuote.push(<span key={i} value={quoteWords[i]} onClick={handleNewCategoryChoice}>{quoteWords[i]} </span>);
     } else {
-      renderedQuote.push(<span key={i} sx={{ color: `${userColor}`, fontFamily: `${userFont}`}}>{quoteWords[i]} </span>);
+      renderedQuote.push(<span key={i}>{quoteWords[i]} </span>);
     }
   };
 
